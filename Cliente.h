27564208b11ea495e7d1d2c4b1de9d984fdcde18
rafaelfb43 <73505641+rafaelfb43@users.noreply.github.com>
2021-03-13 15:57:42 -0700

    Add files via upload

---
+++ Cliente.h
@@ -0,0 +1,11 @@
+#include <string>
+using namespace std;
+class Client
+{
+public:
+    string name;
+    string direction;
+    string rfc;
+    Client();
+    Client(string n, string d, string r);
+};
